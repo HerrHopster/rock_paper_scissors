168cb28110b9936738b25373ffe4d0e76124e007
HerrHopster <hrhopster@protonmail.com>
2025-01-20 06:52:13 +0100

    build pseudo code

---
+++ rock-paper-scissors.js
@@ -0,0 +1,23 @@
+//new function named getComputerChoice that returns "rock", "paper" or "scissors"
+function getComputerChoice() {"rock", "paper", "scissors"}
+//new function named getHumanChoice that returns one of the 3 choices from above
+
+//test the function output with console.log
+
+//new function named humanScore
+
+//new function named computerScore
+
+//initialize humanScore and computerScore with 0
+
+//new function named playRound
+
+//define humanChoice and computerChoice for playRound
+
+//Use the 2 parameters as arguments
+
+//make humanCHoice case sensitive
+
+//console.log e.g “You lose! Paper beats Rock” for playRound
+
+//increment humanScore of computerScore based on the round winner
