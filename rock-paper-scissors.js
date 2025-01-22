84010faa2abac1d03a8464659cce8faa5bb5fc22
HerrHopster <hrhopster@protonmail.com>
2025-01-22 05:40:04 +0100

    create functions that keep score of the human and somputer score

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,17 +1,18 @@
 //new function named getComputerChoice that returns "rock", "paper" or "scissors"
-function getComputerChoice("rock", "paper", "scissors") {}
+function getComputerChoice(rock, paper, scissors) {}
 //new function named getHumanChoice that returns one of the 3 choices from above
-function getHumanChoice("rock", "paper", "scissors")
+function getHumanChoice(rock, paper, scissors) {}
 //test the function output with console.log
-
+console.log(getComputerChoice)
 //new function named humanScore
-
+function humanScore() {}
 //new function named computerScore
-
+function computerScore() {}
 //initialize humanScore and computerScore with 0
-
+humanScore === 0
+computerScore === 0
 //new function named playRound
-
+function playRound() {}
 //define humanChoice and computerChoice for playRound
 
 //Use the 2 parameters as arguments
