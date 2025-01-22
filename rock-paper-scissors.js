a6ad526157674cb7fcd2d52283d55c6d9569e590
HerrHopster <hrhopster@protonmail.com>
2025-01-22 06:43:16 +0100

    change getHumanChoice to a user input funciton

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,5 +1,5 @@
 //new function named getComputerChoice that returns "rock", "paper" or "scissors"
-function getComputerChoice(rock, paper, scissors) {
+function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3)
     switch (randomNumber) {
         case 0:
@@ -11,9 +11,17 @@ function getComputerChoice(rock, paper, scissors) {
     }
 }
 //new function named getHumanChoice that returns one of the 3 choices from above
-function getHumanChoice(rock, paper, scissors) {}
-//test the function output with console.log
-console.log(getComputerChoice)
+function getHumanChoice() {
+    //Lets the user input one of 3 choices
+    let choice = prompt("Please enter rock, paper or scissors:").toLowerCase();
+
+    //checking of the user choice is valid
+    while (choice !=="rock" && choice !=="paper" && choice !== "scissors") {
+        choice = prompt("Your choice is invalid!")
+    }
+    return choice;
+}
+
 //new function named humanScore
 function humanScore() {}
 //new function named computerScore
